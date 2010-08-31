49ffd15d53ef61e570e66e4d237e762b575deb6b
SalesLink Live <admin@raybec.com>
2010-08-31 14:40:03 -0400

    Removed gunzip portion of graphics magick script as tar can do this itself

--- graphics/graphics_magick.sh
+++ graphics/graphics_magick.sh
@@ -2,8 +2,7 @@
 
 cd /tmp
 wget http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.12/GraphicsMagick-1.3.12.tar.gz?use_mirror=voxel
-gunzip GraphicsMagick-1.3.12.tar.gz
-tar xzf GraphicsMagick-1.3.12.tar
+tar xzf GraphicsMagick-1.3.12.tar.gz
 cd GraphicsMagick-1.3.12/
 ./configure --prefix=/usr --enable-shared
 make
