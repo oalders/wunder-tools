7228e2cb837e1409a105bcf2020055ed88255a57
wunderadmin <wunderadmin@healthclubdirectory.com>
2010-09-18 23:20:01 -0500

    Fixes output file name for GraphicsMagick download

--- graphics/graphics_magick.sh
+++ graphics/graphics_magick.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /tmp
-wget http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.12/GraphicsMagick-1.3.12.tar.gz?use_mirror=voxel
+wget http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.12/GraphicsMagick-1.3.12.tar.gz?use_mirror=voxel -O GraphicsMagick-1.3.12.tar.gz
 tar xzf GraphicsMagick-1.3.12.tar.gz
 cd GraphicsMagick-1.3.12/
 ./configure --prefix=/usr --enable-shared
