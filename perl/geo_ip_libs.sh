61f68943000c7cd8b0d303f06651ed2302538cfd
Olaf Alders <olaf@raybec.com>
2010-12-02 21:05:10 +0000

    Adds zlib1g-dev deps to Geo::IP install.  Installer no longer needs to be run as root.

--- perl/geo_ip_libs.sh
+++ perl/geo_ip_libs.sh
@@ -2,13 +2,15 @@
 
 # install/update GeoIP C and Perl libs 
 
+sudo apt-get install zlib1g-dev
+
 cd /tmp
 rm -rf /tmp/GeoIP*
 wget http://geolite.maxmind.com/download/geoip/api/c/GeoIP.tar.gz
 tar xzvf GeoIP.tar.gz
 mv GeoIP-* GeoIP
 cd GeoIP
-./configure && make && make check && make install
+./configure && make && make check && sudo make install
 
 cpanm Geo::IP
 cpanm Geo::IP::PurePerl
