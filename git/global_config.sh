45f693c1353aedcac58e7013575ec37d82e159a3
Olaf Alders <olaf@wundersolutions.com>
2013-11-20 20:45:32 -0500

    Remove some stuff I never use anymore.

--- git/global_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# eliminates "You did not specify any refspecs to push"
-git config --global push.default matching
-
-# eliminates "You asked me to pull without telling me which branch"
-git config --global branch.master.remote origin
-git config --global branch.master.merge refs/heads/master
--- perl/install_rakudo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# installs Rakudo Perl directly from GitHub
-
-git clone git://github.com/rakudo/rakudo.git
-cd rakudo/
-perl Configure.pl --gen-parrot
-make && make test && make install
