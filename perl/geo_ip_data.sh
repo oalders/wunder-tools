ee129e388cade039a4bab67b3396a2b1fb2190ed
Olaf Alders <olaf@wundersolutions.com>
2017-08-27 01:43:38 +0000

    Make GeoIP script less noisy

--- perl/geo_ip_data.sh
+++ perl/geo_ip_data.sh
@@ -1,15 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # download and update GeoIP data
 # this should be run before you install the Geo::IP libs
 
-mkdir -p /usr/share/GeoIP || exit
 cd /usr/share/GeoIP || exit
-rm GeoLiteCity.dat.gz
+rm -f GeoLiteCity.dat.gz
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-rm GeoLiteCity.dat
+rm -f GeoLiteCity.dat
 gunzip GeoLiteCity.dat.gz
 
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
-rm GeoIP.dat
+rm -f GeoIP.dat
 gunzip GeoIP.dat.gz
